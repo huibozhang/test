bf45add0cdc356077f04eaa85bcd2b48640fda30
huibozhang <huibo_zhang@126.com>
2018-05-02 10:18:15 +0800

    tet
    local first commit

---
+++ WebAPI/Filter/Auth.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Security;
+
+namespace WebAPI.Filter
+{
+    public class CheckAuthAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //如果用户方位的Action带有AllowAnonymousAttribute，则不进行授权验证
+            if (actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any())
+            {
+                return;
+            }
+
+            var token = actionContext.Request.Headers.Authorization;
+            string[] userToken = token.ToString().Split('|');
+            //拿着用户id和token到数据库里去做验证
+            var verifyResult = actionContext.Request.Headers.Authorization != null &&  //要求请求中需要带有Authorization头
+                               actionContext.Request.Headers.Authorization.Parameter == "123456"; //并且Authorization参数为123456则验证通过
+
+            //验证不通过
+            if (!verifyResult)
+            {
+                //如果验证不通过，则返回401错误，并且Body中写入错误原因
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, new HttpError("Token 不正确"));
+                //actionContext.Response = actionContext.Request.CreateResponse(new ResponseModel() { });
+            }
+        }
+
+        private bool ValidateTicket(string encryptToken)
+        {
+            bool flag = false;
+            try
+            {
+                //获取数据库Token  
+                //Dec.Models.TicketAuth model = Dec.BLL.TicketAuth.GetTicketAuthByToken(encryptToken);
+                //if (model.Token == encryptToken) //存在  
+                //{
+                //    //未超时  
+                //    flag = (DateTime.Now <= model.ExpireDate) ? true : false;
+                //}
+            }
+            catch (Exception ex) { }
+            return flag;
+        }
+    }
+}
